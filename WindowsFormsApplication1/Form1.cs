566dd4d9747981587ee93593f47e17c0a721539e
saiful619945 <robal.bashar59@gmail.com>
2016-08-28 22:12:27 +0600

    Still Developing

--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -24,15 +24,15 @@ namespace WindowsFormsApplication1
         public Form1()
         {
             InitializeComponent();
-            
+
 
         }
 
-        
+
 
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
-            
+
             richTextBox1.AcceptsTab = true;
 
             MatchCollection keywordMatches = color.match(richTextBox1.Text);
@@ -40,7 +40,7 @@ namespace WindowsFormsApplication1
             int originalIndex = richTextBox1.SelectionStart;
             int originalLength = richTextBox1.SelectionLength;
             Color originalColor = Color.Black;
-            
+
             runButton.Focus();
             richTextBox1.SelectionStart = 0;
             richTextBox1.SelectionLength = richTextBox1.Text.Length;
@@ -78,7 +78,7 @@ namespace WindowsFormsApplication1
             //MatchCollection wordCollection = Regex.Matches(richTextBox1.Text, w.regexExpression);
 
             //countWordLabel.Text = w.wordCounter(wordCollection);
-            
+
             this.openFile();
         }
 
@@ -86,7 +86,7 @@ namespace WindowsFormsApplication1
         {
             //SaveFileDialog saveFile1 = new SaveFileDialog();
 
-          
+
             //saveFile1.DefaultExt = "*.cpp";
             //saveFile1.Filter = "C++ Files|*.cpp";
 
@@ -95,7 +95,7 @@ namespace WindowsFormsApplication1
             //   saveFile1.FileName.Length > 0)
             //{
             //    filepath = saveFile1.FileName;
-                
+
             //    // Save the contents of the RichTextBox into the file.
             //    richTextBox1.SaveFile(saveFile1.FileName, RichTextBoxStreamType.PlainText);
             //}
@@ -112,17 +112,17 @@ namespace WindowsFormsApplication1
 
             try
             {
-                // create the ProcessStartInfo using "cmd" as the program to be run,
-                // and "/c " as the parameters.  & C:\\Users\\student\\Desktop\\i.exe
+                // create the ProcessStartInfo using "cmd" as the program to be run, and "/c " as the
+                // parameters. & C:\\Users\\student\\Desktop\\i.exe
 
                 // Incidentally, /c tells cmd that we want it to execute the command that follows,
                 // and then exit.
                 //System.Diagnostics.ProcessStartInfo procStartInfo =
                 //    new System.Diagnostics.ProcessStartInfo("cmd", "/K cd\\ & g++ -o  C:\\Users\\gg\\Desktop\\main " + filepath );
                 System.Diagnostics.ProcessStartInfo procStartInfo =
-                    new System.Diagnostics.ProcessStartInfo("cmd", "/K cd\\ & g++ -o "+filepath.Substring(0,filepath.Length-4)+" "+ filepath );
-                // The following commands are needed to redirect the standard output.
-                // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                    new System.Diagnostics.ProcessStartInfo("cmd", "/K cd\\ & g++ -o " + filepath.Substring(0, filepath.Length - 4) + " " + filepath);
+                // The following commands are needed to redirect the standard output. This means that
+                // it will be redirected to the Process.StandardOutput StreamReader.
                 procStartInfo.RedirectStandardOutput = true;
                 procStartInfo.UseShellExecute = false;
                 // Do not create the black window.
@@ -138,7 +138,7 @@ namespace WindowsFormsApplication1
                 // Display the command output.
                 build = 1;
 
-                
+
             }
             catch (Exception objException)
             {
@@ -188,7 +188,7 @@ namespace WindowsFormsApplication1
 
 
 
-              
+
         }
         public int number = 0;
         private void keydown(object sender, KeyEventArgs e)
@@ -225,7 +225,7 @@ namespace WindowsFormsApplication1
                         e.Handled = true;
                         break;
                     case Keys.S:
-                 
+
                         e.Handled = true;
                         this.savefile();
                         break;
@@ -236,7 +236,7 @@ namespace WindowsFormsApplication1
                     default:
                         break;
                 }
-            }            
+            }
         }
 
         private void menuStrip1_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
@@ -290,7 +290,7 @@ namespace WindowsFormsApplication1
         private void button2_Click(object sender, EventArgs e)
         {
             //Regex rgx = new Regex(@"\b" + s.RegexExpression + @"\b");
-            Regex rgx = new Regex(@""+s.RegexExpression+"");
+            Regex rgx = new Regex(@"" + s.RegexExpression + "");
             if (s.flag == 1 && rgx.IsMatch(richTextBox1.Text))
             {
 
@@ -315,33 +315,20 @@ namespace WindowsFormsApplication1
         }
         public void savefile()
         {
-            //SaveFileDialog saveFile1 = new SaveFileDialog();
-
-
-            //saveFile1.DefaultExt = "*.cpp";
-            //saveFile1.Filter = "C++ Files|*.cpp";
-
-            //// Determine if the user selected a file name from the saveFileDialog.
-            //if (saveFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
-            //   saveFile1.FileName.Length > 0)
-            //{
-            //    filepath = saveFile1.FileName;
-
-            //    // Save the contents of the RichTextBox into the file.
-            //    richTextBox1.SaveFile(saveFile1.FileName, RichTextBoxStreamType.PlainText);
-            //}
-
+            //assign file path
             string path = @"" + filepath + "";
-            if(!File.Exists(path)){
+            //checking file for its existence
+            if (!File.Exists(path))
+            {
+                //Creating object of save file dialog
                 SaveFileDialog saveFile1 = new SaveFileDialog();
-
-
+                //default save option ".cpp"
                 saveFile1.DefaultExt = "*.cpp";
                 saveFile1.Filter = "C++ Files|*.cpp";
 
                 // Determine if the user selected a file name from the saveFileDialog.
-                if (saveFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK &&
-                   saveFile1.FileName.Length > 0)
+                if (saveFile1.ShowDialog() == System.Windows.Forms.DialogResult.OK
+                    && saveFile1.FileName.Length > 0)
                 {
                     filepath = saveFile1.FileName;
 
@@ -350,26 +337,35 @@ namespace WindowsFormsApplication1
                 }
             }
             else
+                //Write file or overwrite
                 File.WriteAllText(path, richTextBox1.Text);
 
         }
         public void openFile()
         {
+            //object of open file dialog
             OpenFileDialog open = new OpenFileDialog();
+            //show all file in Dialog
             open.Filter = "Text files (*.*)|*.*";
+            //Confirmation
             if (open.ShowDialog() == System.Windows.Forms.DialogResult.OK)
             {
+                //Load file from directory 
                 richTextBox1.LoadFile(open.FileName, RichTextBoxStreamType.PlainText);
+                //save file path from open.filename
                 filepath = open.FileName;
 
             }
-            //MessageBox.Show(filepath.Substring(0,filepath.Length-4));
+            
             //word count after load file
-            //create object of WORDCOUNT
+            //create object of WORDCOUNT class
             WordCount w = new WordCount();
+            //select word from richtextbox1
             w.regexExpression = @"\w+";
+            //wordcollection is array
+            //it matches all text from richtextbox and expression
             MatchCollection wordCollection = Regex.Matches(richTextBox1.Text, w.regexExpression);
-
+            //show count number in word label
             countWordLabel.Text = w.wordCounter(wordCollection);
 
         }
@@ -380,16 +376,16 @@ namespace WindowsFormsApplication1
             {
                 try
                 {
-                    // create the ProcessStartInfo using "cmd" as the program to be run,
-                    // and "/c " as the parameters.  & C:\\Users\\student\\Desktop\\i.exe
+                    // create the ProcessStartInfo using "cmd" as the program to be run, and "/c " as
+                    // the parameters. & C:\\Users\\student\\Desktop\\i.exe
 
-                    // Incidentally, /c tells cmd that we want it to execute the command that follows,
-                    // and then exit.
+                    // Incidentally, /c tells cmd that we want it to execute the command that
+                    // follows, and then exit.
                     System.Diagnostics.ProcessStartInfo procStartInfo =
                         new System.Diagnostics.ProcessStartInfo("cmd", "/c cd\\ & " + filepath.Substring(0, filepath.Length - 4));
 
-                    // The following commands are needed to redirect the standard output.
-                    // This means that it will be redirected to the Process.StandardOutput StreamReader.
+                    // The following commands are needed to redirect the standard output. This means
+                    // that it will be redirected to the Process.StandardOutput StreamReader.
                     procStartInfo.RedirectStandardOutput = true;
                     procStartInfo.UseShellExecute = false;
                     // Do not create the black window.
@@ -414,11 +410,7 @@ namespace WindowsFormsApplication1
                 }
             }
             else
-                MessageBox.Show("Build first",
-    "Warning",
-    MessageBoxButtons.OK,
-    MessageBoxIcon.Warning,
-    MessageBoxDefaultButton.Button1);
+                MessageBox.Show("Build first","Warning",MessageBoxButtons.OK,MessageBoxIcon.Warning,MessageBoxDefaultButton.Button1);
 
         }
 
@@ -445,7 +437,7 @@ namespace WindowsFormsApplication1
                 richTextBox1.SaveFile(saveFile1.FileName, RichTextBoxStreamType.PlainText);
             }
         }
-        
+
 
     }
 }
